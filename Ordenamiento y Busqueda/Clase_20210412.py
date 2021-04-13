155fd25cb190aee9fae02a849161ccf88b8ca029
BrunoFer1994 <brunofernandez2008@gmail.com>
2021-04-12 22:49:11 -0300

    Clase ordenamiento y busqueda.

---
+++ Ordenamiento y Busqueda/Clase_20210412.py
@@ -0,0 +1,38 @@
+datos = [11, 3, 81, 7, 45, 20, 4, 5, 10, 25, 30, 22]
+
+def quicksort(vector, inicio, fin):
+    primero = inicio
+    ultimo = fin -1
+    pivote = fin
+    while(primero < ultimo):
+        while(vector[primero]<vector[pivote] and primero <= ultimo):
+            primero += 1
+        while(vector[ultimo]>vector[pivote] and ultimo >= primero):
+            ultimo -= 1
+        
+        if(primero < ultimo):
+            vector[primero], vector[ultimo] = vector[ultimo], vector[primero]
+    if(vector[pivote]<vector[primero]):
+        vector[primero], vector[pivote] = vector[pivote], vector[primero]
+
+    if(inicio < primero):
+        quicksort(vector, inicio, primero -1)
+    if(fin > primero):
+        quicksort(vector, primero + 1, fin)
+
+def busqueda_binaria(vector, buscado, inicio, fin):
+    if(inicio<= fin):
+        medio = (inicio + fin) // 2
+        if(vector[medio] == buscado):
+            return medio
+        elif(vector[medio] < buscado):
+            return busqueda_binaria(vector, buscado, medio+1, fin)
+        else:
+            return busqueda_binaria(vector, buscado, inicio, medio-1)
+    else:
+        return -1
+
+
+quicksort(datos,0, len(datos)-1)
+print(datos)
+print(busqueda_binaria(datos, 8, 0, len(datos)-1))
