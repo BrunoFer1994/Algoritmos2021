478bb474a5389b2b6464188fccb5df714fc3b173
BrunoFer1994 <brunofernandez2008@gmail.com>
2021-05-01 20:07:10 -0300

    Ejercicios para entregar

---
+++ Pilas/tp2_ej14_pila_ordenada.py
@@ -0,0 +1,3 @@
+"""14. Realizar un algoritmo que permita ingresar elementos en una pila, y que estos queden ordenados de forma creciente. Solo puede utilizar una pila auxiliar como estructura extra 
+–no sepueden utilizar métodos de ordenamiento–."""
+
---
+++ Pilas/tp2_ej22_bitacoras.py
@@ -0,0 +1,8 @@
+"""22. Se recuperaron las bitácoras de las naves del cazarrecompensas Boba Fett y Din Djarin (The Mandalorian), las cuales se almacenaban en una pila (en su correspondiente nave) en 
+cada misión de caza que emprendió, con la siguiente información: planeta visitado, a quien capturó, costo de la recompensa. Resolver las siguientes actividades:
+    a. mostrar los planetas visitados en el orden que hicieron las misiones cada uno de los cazzarrecompensas;
+    b. determinar cuántos créditos galácticos recaudo en total cada cazarrecompensas y de estos quien obtuvo mayor fortuna;
+    c. determinar el número de la misión –es decir su posición desde el fondo de la pila– en la que Boba Fett capturo a Han Solo, suponga que dicha misión está cargada;
+    d. indicar la cantidad de capturas realizadas por cada cazarrecompensas."""
+
+
